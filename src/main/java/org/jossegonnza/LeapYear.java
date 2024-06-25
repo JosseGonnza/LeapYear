1a49a4c16acf33adb9012570524355765c7c80fd
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-25 18:54:56 +0100

    🔴 First test RED!

--- src/main/java/org/jossegonnza/LeapYear.java
+++ src/main/java/org/jossegonnza/LeapYear.java
@@ -1,4 +1,8 @@
 package org.jossegonnza;
 
 public class LeapYear {
+
+    public boolean isLeapYear(int year) {
+        return true;
+    }
 }
--- src/test/java/org/jossegonnza/LeapYearTest.java
+++ src/test/java/org/jossegonnza/LeapYearTest.java
@@ -1,7 +1,13 @@
 package org.jossegonnza;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 
 class LeapYearTest {
-
+    @Test
+    public void is_not_a_leap_year_if_it_is_not_divisible_by_4() {
+        LeapYear leapYear = new LeapYear();
+        Assertions.assertThat(leapYear.isLeapYear(1997)).isEqualTo(false);
+    }
 }
