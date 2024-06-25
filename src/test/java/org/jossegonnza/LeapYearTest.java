89dcca7bdb866643f3dabbe9ea8f2884108c92d2
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-25 18:07:37 +0100

    🔴 Should return true if is divisible by 4

--- src/test/java/org/jossegonnza/LeapYearTest.java
+++ src/test/java/org/jossegonnza/LeapYearTest.java
@@ -1,7 +1,16 @@
 package org.jossegonnza;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class LeapYearTest {
+
+    LeapYear leapYear = new LeapYear();
+
+    @Test
+    public void should_return_true_if_divisible_by_4() {
+
+        Assertions.assertThat(4).isEqualTo("true");
+    }
 
 }
